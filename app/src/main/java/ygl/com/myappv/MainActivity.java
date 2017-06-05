23080a0cd29f714d4ca473acc856857b6e735ac4
juju <juju75_20@hotmail.fr>
2017-06-05 16:46:49 +0200

    Merge branch 'feature' of https://github.com/juju75dev/myAppV
    # Conflicts:
    #	app/src/main/java/ygl/com/myappv/MainActivity.java

--- app/src/main/java/ygl/com/myappv/MainActivity.java
+++ app/src/main/java/ygl/com/myappv/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Log.d("Vince","Popopo");
+        Log.d("Vince","trtrtrtr");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
