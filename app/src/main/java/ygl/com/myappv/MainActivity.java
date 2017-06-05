6b612990124d2c9ec636b1c9f31216eebf2ce551
juju <juju75_20@hotmail.fr>
2017-06-05 14:37:25 +0200

    rr-orororo

--- app/src/main/java/ygl/com/myappv/MainActivity.java
+++ app/src/main/java/ygl/com/myappv/MainActivity.java
@@ -3,7 +3,6 @@ package ygl.com.myappv;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -15,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        Log.d("rrrrrrrr","flaflffa");
 
         /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
