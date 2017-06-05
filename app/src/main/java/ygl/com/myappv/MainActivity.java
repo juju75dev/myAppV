7315fca732492c226b715d4e65228951ce87c620
juju <juju75_20@hotmail.fr>
2017-06-05 15:40:00 +0200

    commitFromDownloadedVersion

--- app/src/main/java/ygl/com/myappv/MainActivity.java
+++ app/src/main/java/ygl/com/myappv/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Log.d("rrrrrrrr","rrrrrr");
+        Log.d("rrrrrrrr","fffff");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
